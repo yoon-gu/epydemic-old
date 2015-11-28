582303199f8f8374b2fe08c99e4bc77025ee3c08
Yoonku Hwang <yz0624@gmail.com>
2015-11-29 02:04:32 +0900

    add draft model class

--- epydemic/models/models.py
+++ epydemic/models/models.py
@@ -53,3 +53,66 @@ def seir(gamma = 0.1, beta = 0.5, mu = 0.2, a = 1):
 
 	"""
 	print "SEIR Model"
+
+
+
+if __name__ == '__main__':
+	import scipy.integrate as spi
+	import numpy as np
+	# parameters for each model
+	beta, gamma = 1.42, 0.143
+	parameter = {'beta' : beta, 'gamma' : gamma}
+	# initial values for each model
+	S0, I0, R0 = 1.0 - 1.0e-6, 1.0e-6, 0.0
+	INPUT = (S0, I0, R0)
+
+	# time range(step, end)
+	TS, ND = 1, 10
+	
+	# the model
+	def diff_eqs(INP,t):
+	    '''The main set of equations'''
+	    Y = np.zeros((3))
+	    V = INP
+	    Y[0] = - beta * V[0] * V[1]
+	    Y[1] = beta * V[0] * V[1] - gamma * V[1]
+	    Y[2] = gamma * V[1]
+	    return Y   # For odeint
+
+	# solve model    
+	t_start = 0.0; t_end = ND; t_inc = TS
+	t_range = np.arange(t_start, t_end + t_inc, t_inc)
+	RES = spi.odeint(diff_eqs, INPUT, t_range)
+	
+	class model(object):
+		import scipy.integrate as spi
+		import numpy as np
+		"""
+		docstring for models
+		"""
+		def __init__(self, eqn):
+			self.equation = eqn
+
+		def __str__(self):
+			return "Hello"
+
+		def set_parameters(self, param):
+			self.parameter = param
+
+		def set_initial_values(self, init_val):
+			self.initial_value = init_val
+
+		def solve(self):
+			# solve model    
+			t_start = 0.0; t_end = 10; t_inc = 1
+			t_range = np.arange(t_start, t_end + t_inc, t_inc)
+			RES = spi.odeint(diff_eqs, self.initial_value, t_range)
+			return RES
+			
+	md = model(diff_eqs)
+	md.set_parameters(parameter)
+	md.set_initial_values(INPUT)
+
+	print md.parameter
+	print md.initial_value
+	print md.solve()
