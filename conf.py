27701cabf107a0e149f5ca5916b6372d947b97e4
Yoonku Hwang <yz0624@gmail.com>
2015-11-28 09:22:17 +0900

    typo

--- conf.py
+++ conf.py
@@ -300,6 +300,6 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
             return Mock()
 
-MOCK_MODULES = ['PyQt4', 'pyqtgraph', ,'pyqtgraph.parametertree', 
+MOCK_MODULES = ['PyQt4', 'pyqtgraph', 'pyqtgraph.parametertree', 
                 'pyqtgraph.parametertree.parameterTypes', 'numpy']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
