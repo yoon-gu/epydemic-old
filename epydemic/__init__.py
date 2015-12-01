d1d9ae87da9399834960b408d3bf681d3ad9d25a
Yoonku Hwang <yz0624@gmail.com>
2015-12-02 01:02:36 +0900

    grammar mistake

--- epydemic/__init__.py
+++ epydemic/__init__.py
@@ -2,7 +2,7 @@
 Epydemic Module
 ===============
 
-``epydemic`` is a software package written by ``Python`` for mathematical epidemic modeling. There are three submodules.
+``epydemic`` is a software package written in ``Python`` for mathematical epidemic modeling. There are three submodules.
 
 * ``models`` contains epidemic modeling ordinary differential equation(ODE).
 * ``estimations`` performs parameter estimations given real world data.
