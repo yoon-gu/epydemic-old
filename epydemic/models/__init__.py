9c18a4169dd0637119ea163c8c618d9b847c05f0
Yoonku Hwang <yz0624@gmail.com>
2015-12-03 22:50:39 +0900

    models subpacakge 문서 추가

--- epydemic/models/__init__.py
+++ epydemic/models/__init__.py
@@ -2,35 +2,15 @@
 Models
 ======
 
-Example NumPy style docstrings.
+There are two types model `class`es.
 
-This module demonstrates documentation as specified by the `NumPy
-Documentation HOWTO`_. Docstrings may extend over multiple lines. Sections
-are created with a section header followed by an underline of equal length.
+#. Pre-defined `model`
+#. Custom `model`
 
-Example
--------
-Examples can be given using either the ``Example`` or ``Examples``
-sections. Sections support any reStructuredText formatting, including
-literal blocks::
-
-    $ python example_numpy.py
-
-
-Section breaks are created with two blank lines. Section breaks are also
-implicitly created anytime a new section starts. Section bodies *may* be
-indented:
-
-Notes
------
-    This is an example of an indented section. It's like any other section,
-    but the body is indented to help it stand out from surrounding text.
-
-If a section is indented, then a section break is created simply by
-resuming unindented text.
-
-.. _NumPy Documentation HOWTO:
-   https://github.com/numpy/numpy/blob/master/doc/HOWTO_DOCUMENT.rst.txt
+Built-in Models
+---------------
+* SIR
+* SEIR
 
 """
 
