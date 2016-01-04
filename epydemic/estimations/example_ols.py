064faaacb0779c1909d765225eb44ac577b85ccf
Yoonku Hwang <yz0624@gmail.com>
2016-01-05 01:48:38 +0900

    key-value parameter

--- epydemic/estimations/example_ols.py
+++ epydemic/estimations/example_ols.py
@@ -4,20 +4,6 @@ from numpy.random import randn
 import scipy.optimize
 from numpy import linalg as LA
 
-# Parameters
-beta = 5e-6
-gamma = 5e-1
-S0 = 3.5e5
-I0 = 9e1
-R0 = 0.
-theta = array([S0,I0,beta,gamma])
-
-# Discrete Time Domain
-t_start = 0
-t_end = 14
-t_inc = 0.5
-t_range = arange(t_start, t_end + t_inc, t_inc)
-
 # SIR Model
 def sir(INP, t, params):
     beta = params['beta']
@@ -29,9 +15,38 @@ def sir(INP, t, params):
     Y[2] = gamma * V[1]
     return Y # For odeint
 
+def ols_cost(theta, timep, Y):
+    initial = (theta[0], theta[1], 0.)
+    sol = odeint(sir, initial, t_range,
+        args = ({'beta':theta[2], 'gamma':theta[3]}, ))
+    S, I = sol[:,0], sol[:,1]
+    SI = S * I
+    Z = 0.5 * ( t_range[1:] - t_range[:-1] ) * beta * ( SI[:-1] + SI[1:] )
+    return LA.norm(Z - Y)
+
+# Parameters
+beta = 5e-6
+gamma = 5e-1
+S0 = 3.5e5
+I0 = 9e1
+R0 = 0.
+t_start = 0
+t_end = 14
+t_inc = 0.5
+t_range = arange(t_start, t_end + t_inc, t_inc)
+
+parameter = {
+        'beta' : 5e-6, 
+        'gamma' : 5e-1, 
+        'S0' : 3.5e5, 
+        'I0' : 9e1, 
+        'R0' : 0., 
+        't_range' : t_range, 
+        }
+
 # Solve Ordinary Differential Equation
 sol = odeint(sir, (S0, I0, R0), t_range,
-    args = ({'beta':beta, 'gamma':gamma}, ))
+    args = (parameter, ))
 
 S, I = sol[:,0], sol[:,1]
 SI = S * I
@@ -43,18 +58,10 @@ c = alpha * min(Z)
 Y = Z + c * randn(len(Z))
 
 # Getting noisy parameter, intentionally
+theta = array([S0,I0,beta,gamma])
 theta0 = 1.25 * theta
 
 # Estimating parameter
-def L(theta, timep, Y):
-    initial = (theta[0], theta[1], 0.)
-    sol = odeint(sir, initial, t_range,
-        args = ({'beta':theta[2], 'gamma':theta[3]}, ))
-    S, I = sol[:,0], sol[:,1]
-    SI = S * I
-    Z = 0.5 * ( t_range[1:] - t_range[:-1] ) * beta * ( SI[:-1] + SI[1:] )
-    return LA.norm(Z - Y)
-
-theta_ols = scipy.optimize.fmin(func=L, x0=theta0, args=(t_range, Y,))
+theta_ols = scipy.optimize.fmin(func=ols_cost, x0=theta0, args=(t_range, Y,))
 
 print theta_ols
